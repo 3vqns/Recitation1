72d027535a4142b66ca011f09d23f19b6d7fbdc3
3vqns <earmantrading23@students.desu.edu>
2024-01-26 12:35:51 -0500

    Completed

--- src/main/java/BasicInput.java
+++ src/main/java/BasicInput.java
@@ -3,18 +3,39 @@ import java.util.Scanner;
 public class BasicInput {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        int userInt = 0;
-        double userDouble = 0.0;
+        int userInt;
+        double userDouble;
         // TODO Define char and string variables similarly
+        char userChar;
+        String userString;
 
         System.out.println("Enter integer:");
         userInt = scnr.nextInt();
 
         // TODO (1): Finish reading other items into variables, then output the four values on a single line separated by a space
 
+        System.out.println("Enter double:");
+        userDouble = scnr.nextDouble();
+
+        System.out.println("Enter character:");
+        userChar = scnr.next().charAt(0);
+
+        System.out.println("Enter string:");
+        userString = scnr.next();
+
+        System.out.print(userInt);
+        System.out.print(" " + userDouble);
+        System.out.print(" " + userChar);
+        System.out.println(" " + userString);
+
         // TODO (2): Output the four values in reverse
+        System.out.print(userString);
+        System.out.print(" " + userChar);
+        System.out.print(" " + userDouble);
+        System.out.println(" " + userInt);
 
         // TODO (3): Cast the double to an integer, and output that integer
-
+        int newUserDouble = (int)(userDouble);
+        System.out.println(userDouble+" cast to an integer is "+newUserDouble);
     }
 }
